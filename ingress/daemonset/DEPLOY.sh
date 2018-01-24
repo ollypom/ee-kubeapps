3c3c51ba6ee13eec4e10fc0a7d60458b610cb9fa
ollypom <oppomeroy@gmail.com>
2018-01-24 10:26:30 +0000

    Swapped back to Kubectl

--- ingress/daemonset/DEPLOY.sh
+++ ingress/daemonset/DEPLOY.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Label Manager Nodes"
-ukp label nodes ip-172-31-16-8 opnoderole=loadbalancer
+kubectl label nodes ip-172-31-16-8 opnoderole=loadbalancer
 
 echo "Deploying all the yaml"
-ukp apply -f .
+kubectl apply -f .
