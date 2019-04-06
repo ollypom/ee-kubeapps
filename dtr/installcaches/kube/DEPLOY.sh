7b00aafca6af04e5e3bb40ca3f82d5e8a526efb0
ollypom <olly@docker.com>
2019-04-06 20:03:18 +0000

    Added DTR Cache yamls

---
+++ dtr/installcaches/kube/DEPLOY.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# You need to create a role for your service account!! :( 
+
+#echo "Label Manager Nodes"
+#kubectl label nodes ip-172-31-16-8 opnoderole=loadbalancer
+
+# Create Secrets
+
+kubectl create secret generic dtr-certs --from-file=certs/dtr.cert.pem  --from-file=certs/cache.cert.pem --from-file=certs/cache.key.pem
+
+# Create Config map
+
+kubectl create configmap dtr-cache-config --from-file=config.yml
+
+#echo "Deploying all the yaml"
+#kubectl apply -f .
+
+# You can verify 
+
+curl -X GET -k https://dtr-cache/v2/_catalog
+
+# Define a DTR cache within DTR
+
+curl -X POST "https://dtr.olly.dtcntr.net/api/v0/content_caches" -H "accept: application/json" -H "content-type: application/json" -d "{ \"host\": \"https://dtr-cache.dtr.svc.cluster.local\", \"name\": \"aks\"}"
+
+# Create a Registry Secret for Jeff in Kube
+
+kubectl create secret docker-registry dtrcreds --docker-server=dtr.olly.dtcntr.net --docker-username=jeff --docker-password=docker123 --docker-email=jeff@fake.com
+
+
+
